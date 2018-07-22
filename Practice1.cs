79a65b309b7bc7022c17e1bfc6a2d9ce803e0e92
michael rodsky <mrodsky@gmail.com>
2018-07-22 11:21:28 -0400

    first commit

---
+++ Practice1.cs
@@ -0,0 +1,90 @@
+//for each variable DECLARATION,
+// write the type and name of each variable.  (HINT: some declarations may NOT be valid!)
+                              //TYPE   |   //NAME
+char x = ' ';       
+var silly = 's';
+var char = "s";
+double penny = 10.22D;
+People people = p;
+var string = new Person();
+x = "hello";
+new Pizza();
+var y = 40.00M;
+var string = 's';
+float decimal = 32.00F;
+decimal float = 32.00M;
+ulong number = -12;
+Pizza pizza = new Pizza();
+Radio string = new Radio();
+Cheese c; 
+List<cheese> Cheeses; 
+Dictonary<Topping> _T;
+LinkedList<Pizza> P;
+List<T> Pizza;
+
+/*
+What is the value of B after the loop
+ */
+int b = 100;
+for (b = 10; b < 56; b+=2)
+{
+
+}
+
+List<Cheese> cheeses = new List<Cheese>();
+for (b = 1; b < cheeses.Length ; b ++ )
+{
+
+}
+
+
+for (int b = 5, j = 0; b <= b - j; b+=2, j--)
+{
+
+}
+
+//declare an array of characters with 10 elements;
+
+
+
+//declare an array of integers with 1 element;
+
+
+// declare a jagged arary of integers 
+
+
+//declare a 3x3 multidimensinal array of int and fill it with it with 1-9
+
+
+// given this list add x to the list 10 times.
+List<int> numbers = new List<int>();
+
+
+// given the following list; write a code statement that will find ' ' on the list
+List<string> classnames = new List<string> 
+{
+  "michael", "javier", "cady", "curtis", "raul", "fred", "todd"
+};
+foreach (string item in classnames)
+{
+  if(classnames.item(x => x.Contains("cady"))
+   return item;
+}
+classnames.Where(x => x.Contains("cady").First();
+
+
+/*********************************PROGRAMMING*********************************************** */
+
+// should input a string in a console
+// Write a program that will count the number of spaces in a string;
+
+//*********************************************************************************************
+ 
+ // program should input 2 numbers in the console. write a program that will raise number 1 
+ // to the power of number2;
+
+ //************************************************************************************** */
+
+ // Write a program that will determine if a number is prime or not 
+ // HINT: a number is prime if it is not divisible by any number except 1 and itself
+ // Write most of your program in a CLASS LIBRARY project. 
---
+++ Practice1_Answers.cs
@@ -0,0 +1,11 @@
+// given the following list; write a code statement that will find ' ' on the list
+List<string> classnames = new List<string> 
+{
+  "michael", "javier", "cady", "curtis", "raul", "fred", "todd"
+};
+foreach (string item in classnames)
+{
+  if(classnames.item(x => x.Contains("cady"))
+   return item;
+}
+classnames.Where(x => x.Contains("cady").First();
